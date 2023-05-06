66394c62ad67204f0a4a8f66c0f488c3d3dfa3f9
enkhe-D <enkhedeny07@gmail.com>
2023-05-06 12:54:08 +0200

    Premier jet de mon portfolio

---
+++ script.js
@@ -0,0 +1,25 @@
+// function displayCard() {
+//   let header = {
+//     title: "Portfolio",
+//     img: {
+//       src: "../images/moi.jpg",
+//       alt: "photo de profil",
+//     },
+//     intro: "Plonge dans mon monde pendant quelques milliers de secondes",
+//     date: "06/05/2023",
+//   };
+
+//   let cardsHeader = "";
+
+//   cardsHeader += `<div>
+//                       <h1 class="title">${header.title}</h1>
+//                       <img class="picture" src="${header.img.src}" alt="${header.img.alt}"/>
+//                       <p class="intro">${header.intro}</p>
+//                       <p class="date">${header.date}</p>
+//                     </div>`;
+
+//   document.querySelector("body").innerHTML = cardsHeader;
+// }
+// displayCard();
+
+function loader() {}
